95b17953c33c53362f15b75e0def245daa14cc8e
ayva <olga.ayva@gmail.com>
2015-09-08 08:59:48 -0700

    assignment

--- just_clicking/js/model.js
+++ just_clicking/js/model.js
@@ -5,7 +5,7 @@ JC.model = {
   // Holds squares that are currently lit up on the board.
 
   activeSquares: [],
-
+  realScore: 0,
   // Randomly generates a pair of coordinates.
 
   pickSquare: function(){
@@ -14,6 +14,7 @@ JC.model = {
                       y: Math.floor(Math.random() * 4)
                     };
                 JC.model.activeSquares.push( square );
+                
                 JC.view.lightUpSquare( square );
               },
 
@@ -22,7 +23,7 @@ JC.model = {
                 for( var i = 0; i < this.activeSquares.length; i++ ){
                   var testSquare = this.activeSquares[i];
                   if( testSquare.x === x && testSquare.y === y ) {
-                    this.incrementUpScore();
+                    JC.view.incrementUpScore();
                     this.activeSquares.splice(i, 1); 
                     break;                    
                   };
@@ -30,11 +31,5 @@ JC.model = {
                 };
               },
 
-  // Adds a random number between 5 and 15 to the score.
-  incrementUpScore: function(){
-                      var $scoreSpan = $( "#score" )
-                      var score = parseInt( $scoreSpan.text() );
-                      score += ( Math.floor(Math.random() * 15) + 5);
-                      $scoreSpan.text( score );
-                    }
+  
 };
--- just_clicking/js/view.js
+++ just_clicking/js/view.js
@@ -1,45 +1,75 @@
 var JC = JC || {};
 
-JC.view = {
+JC.view = (function(){
+  var realScore = 0;
 
-  init: function(){
-          this.fillClickZone();
-          this.attachListeners();
-        },
+  function init(){
+
+          JC.view.fillClickZone();
+          JC.view.attachListeners();
+        }
 
   // These three function put the board on the page.
 
-  fillClickZone: function(){
+  function fillClickZone(){
                    for (var i = 0; i < 4; i++) {
-                     this.addRowToClickZone(i);
+                     JC.view.addRowToClickZone(i);
                    }
-                 },
+                 }
 
-  addRowToClickZone: function( y ){
+  function addRowToClickZone( y ){
                        $row = $( "<div class='row'></div>" )
                        $( '#click-zone' ).append( $row )
                        for (var i = 0; i < 4; i++) {
-                         this.addClickerToRow( $row, i, y )
+                         JC.view.addClickerToRow( $row, i, y )
                        }
-                     },
+                     }
 
-  addClickerToRow: function( row, x, y ){
+  function addClickerToRow( row, x, y ){
                      row.append( "<div class='clicker' data-x='" + x + "' data-y='" + y + "'></div>" )
-                   },
+                   }
 
   // This sets up the click listener on each square.
 
-  attachListeners: function(){
+  function attachListeners(){
                      $( ".clicker" ).click( function(){
                        var $square = $( this )
                        $square.removeClass( "lit" );
                        JC.model.scoreClick($square.data().x, $square.data().y);
-                   })},
+                   })}
 
   // This turns a square red.
 
-  lightUpSquare: function( square ){
+  function lightUpSquare( square ){
                    var $square = $( ".clicker[data-x='" + square.x + "'][data-y='" + square.y + "']" );
                    $square.addClass( "lit" );
                  }
-};
+
+  // Adds a random number between 5 and 15 to the score.
+  function incrementUpScore(){
+                      var $scoreSpan = $( "#score" )
+                      var score = parseInt( $scoreSpan.text() );
+
+                      //Controls editing score DOM
+                      if (JC.view.realScore === score){
+                        JC.view.realScore += ( Math.floor(Math.random() * 15) + 5);
+                      } else {
+                        JC.view.realScore = 0
+                        $scoreSpan.text( JC.view.realScore );
+                      }
+                      
+                      $scoreSpan.text( JC.view.realScore );
+                    }
+
+  return {
+    incrementUpScore:incrementUpScore,
+    realScore: realScore,
+    init:init,
+    fillClickZone:fillClickZone,
+    lightUpSquare:lightUpSquare,
+    addRowToClickZone:addRowToClickZone,
+    addClickerToRow:addClickerToRow,
+    attachListeners:attachListeners
+
+  }
+})();
